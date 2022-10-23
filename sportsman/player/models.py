4780dc7052dbc292d313b23af713b0c15b3cd30c
Iksanov Roman <iksanov1998@inbox.ru>
2022-10-23 13:38:17 +0500

    Сформированы URL-адреса
    Сформировал URL-адреса у главного меню, а также у каждого отдельного
    поста. В качестве функций-отображения используются заглушки. В главном
    меню используются ссылки из списка menu, а для постов используются
    ссылки через метод get_absolute_url

--- sportsman/player/models.py
+++ sportsman/player/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
+
 class Player(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
@@ -10,3 +13,6 @@ class Player(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_id': self.pk})
--- sportsman/player/views.py
+++ sportsman/player/views.py
@@ -2,21 +2,45 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from .models import *
 
-menu = ['Главная страница', 'О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu = [{'title': 'Главная страница', 'url_name': 'home'},
+        {'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'}
+        ]
+
 
 # Create your views here.
 def index(request):
     posts = Player.objects.all()
-    return render(request, 'player/index.html', {'posts': posts, 'menu': menu, 'title':'Главная страница'})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+
+    return render(request, 'player/index.html', context=context)
+
 
 def about(request):
-    return render(request, 'player/about.html', {'menu': menu, 'title':'О сайте'})
+    return render(request, 'player/about.html', {'menu': menu, 'title': 'О сайте'})
 
-def categories(request, catid):
-    return HttpResponse(f"Категории спортсменов. Страница №{catid}")
 
-def archive(request, year):
-    return HttpResponse(f"Страница с годом {year}")
+def add_page(request):
+    return HttpResponse(f"Добавить статью")
+
+
+def contact(request):
+    return HttpResponse(f"Обратная связь")
+
+
+def login(request):
+    return HttpResponse(f"Авторизация")
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с идентификатором {post_id}")
+
 
 def PageNotFound(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
