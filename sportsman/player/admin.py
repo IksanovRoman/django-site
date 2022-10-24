0c2aba6f7c2ad909f439119dd8ae7aeffe788f6f
Iksanov Roman <iksanov1998@inbox.ru>
2022-10-24 11:05:30 +0500

    Настройка админ панели и отображение фото
    Настроил админ панель, зарегистрировал два приложения, добавил к постам
    фотографии и вывел их через шаблон. Согласно уроку 2.3

--- sportsman/player/admin.py
+++ sportsman/player/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import *
+
+
 # Register your models here.
+class PlayerConfig(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'photo', 'time_created', 'time_update')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_filter = ('time_created', )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', )
+
+
+admin.site.register(Player, PlayerConfig)
+admin.site.register(Category, CategoryAdmin)
--- sportsman/player/models.py
+++ sportsman/player/models.py
@@ -5,12 +5,12 @@ from django.urls import reverse
 
 
 class Player(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photo/%Y/%m/%d/")
-    time_created = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    content = models.TextField(blank=True, verbose_name='Статья')
+    photo = models.ImageField(upload_to="photo/%Y/%m/%d/", verbose_name='Фотография')
+    time_created = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     def __str__(self):
         return self.title
@@ -18,11 +18,22 @@ class Player(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        verbose_name = 'Cпортсмен'
+        verbose_name_plural = 'Cпортсмены'
+        ordering = ['time_created']
+
+
 class Category(models.Model):
-    name = models.CharField(max_length=255, db_index=True)
+    name = models.CharField(max_length=255, db_index=True, verbose_name='Категория')
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
