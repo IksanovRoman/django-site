695eb7a4f999f79e259301abcdd2001a976648c8
Iksanov Roman <iksanov1998@inbox.ru>
2022-10-24 21:15:40 +0500

    Добавил кнопку регистрации

--- sportsman/player/urls.py
+++ sportsman/player/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('add_page/', AddPage.as_view(), name='add_page'),
     path('contact/', contact, name='contact'),
+    path('register/', RegisterUser.as_view(), name='register'),
     path('login/', login, name='login'),
     path('post/<slug:post_slug>', ShowPost.as_view(), name='post'),
     path('category/<slug:cat_slug>', PlayerCategory.as_view(), name='category'),
--- sportsman/player/views.py
+++ sportsman/player/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, get_object_or_404, redirect
@@ -66,6 +67,16 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
 
 def contact(request):
     return HttpResponse(f"Обратная связь")
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'player/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return dict(list(context.items()) + list(c_def.items()))
 
 
 def login(request):
